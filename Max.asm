0f15bf7ae09f5db8d69747bd7b3a8d6f7ab1f175
itsmebabysmiley <54635089+itsmebabysmiley@users.noreply.github.com>
2020-08-19 11:48:30 +0700

    Update Max.asm

--- Max.asm
+++ Max.asm
@@ -1,22 +1,23 @@
-
-
-;nasm -f obj mmax.asm
-;bcc32 first.obj driver.obj asm_io.obj
-
+; How to complie this .asm file to .exe?
+; copy just.asm to NASM folder.
+; open cmd cd to NASM folder.
+; type >>> nasm -f obj ifelse.asm && bcc32 ifelse.obj driver.obj asm_io.obj && ifelse
 %include "asm_io.inc"
 segment _DATA public align=4 class=DATA use32
 
-message1 db "Enter a number: ",0
-message2 db "Enter another number: ", 0
-message3 db "Enter another number: ", 0
-message4 db "The larger number is: ", 0
+message1 db "Enter 1st number: ",0
+message2 db "Enter 2nd number: ", 0
+message3 db "Enter 3rd number: ", 0
+
+result   db "The result is: ",0
+
 
 segment _BSS public align=4 class=BSS use32
 
-input1	resd    1                ; first number entered
-input2	resd	1		 ; second number 
-input3  resd    1    
-input4  resd    1
+input1	 resd    1              ; first number entered
+input2   resd    1
+input3   resd    1
+max      resd    1
 
 group DGROUP _BSS _DATA
 
@@ -34,41 +35,33 @@ _asm_main:
         mov     eax, message2     ; print out second message
         call    print_string
         call    read_int          ; input second number
-        xor     ebx, ebx          ; ebx = 0
-        cmp     eax, [input1]     ; compare second and first number
-        setg    bl                ; ebx = (input2 > input1) ?          1 : 0
-        neg     ebx               ; ebx = (input2 > input1) ? 0xFFFFFFFF : 0
-        mov     ecx, ebx          ; ecx = (input2 > input1) ? 0xFFFFFFFF : 0
-        and     ecx, eax          ; ecx = (input2 > input1) ?     input2 : 0
-        not     ebx               ; ebx = (input2 > input1) ?          0 : 0xFFFFFFFF
-        and     ebx, [input1]     ; ebx = (input2 > input1) ?          0 : input1
-        or      ecx, ebx          ; ecx = (input2 > input1) ?     input2 : input1
-        mov     [input3],ecx
+        mov     [input2], eax
 
-        mov     eax, message3     ; print out third message
+        mov     eax, message3     ; print out second message
         call    print_string
-        call    read_int          ; input third number
-        xor     ebx, ebx          ; ebx = 0
-        cmp     eax, [input3]     ; compare second and first number
-        setg    bl                ; ebx = (input2 > input1) ?          1 : 0
-        neg     ebx               ; ebx = (input2 > input1) ? 0xFFFFFFFF : 0
-        mov     ecx, ebx          ; ecx = (input2 > input1) ? 0xFFFFFFFF : 0
-        and     ecx, eax          ; ecx = (input2 > input1) ?     input2 : 0
-        not     ebx               ; ebx = (input2 > input1) ?          0 : 0xFFFFFFFF
-        and     ebx, [input3]     ; ebx = (input2 > input1) ?          0 : input1
-        or      ecx, ebx          ; ecx = (input2 > input1) ?     input2 : input1
-        mov     [input4],ecx
+        call    read_int          ; input second number
+        mov     [input3], eax
+
+        ;compare
+        mov     eax, [input1]     ; eax = input1
+        cmp     eax, [input2]     ; if eax > input2
+        jg      compare             ;     jump when eax > input2
+        mov     eax, [input2]     ; else eax = input2 (input2 is greater)
+
+compare:
+        cmp     eax, [input3]     ; if eax > input3
+        jg      exit              ;     jump when eax > input3
+        mov     eax, [input3]
 
 
-        mov     eax, message4     ; print out result
-        call    print_string
-        mov     eax, [input4]
-        call    print_int
-        call    print_nl
-
+exit:   
+        mov    [max] , eax
+        mov    eax, result
+        call   print_string       ;print 'The result is: '
+        mov    eax, [max]
+        call   print_int
+        call   print_nl
         popa
         mov     eax, 0            ; return back to C
         leave                     
         ret
-
-
